8eac552520fcb627ef186b2d854e5f6d63796c29
Benjamin Carlson <benjamin.carlson@maine.edu>
2015-04-27 07:48:35 -0700

    using another time to take care of positive half cycle

--- zero_crossing.c
+++ zero_crossing.c
@@ -4,13 +4,27 @@
 #include "inc/hw_memmap.h"
 #include "driverlib/rom_map.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
 
 #include "triac_mgmt.h"
 
 void __int_ZERO(void)
 {
     uint32_t status = GPIOIntStatus(GPIO_PORTN_BASE, true);
-    GPIOIntClear(GPIO_PORTN_BASE, status);
+    if (status)
+    {
+    	GPIOIntClear(GPIO_PORTN_BASE, status);
+
+    	// Start the positive half cycle timer
+    	TimerEnable(TIMER2_BASE, TIMER_A);
+    }
+
+    status = TimerIntStatus(TIMER2_BASE, true);
+    if (status)
+    {
+    	TimerIntClear(TIMER2_BASE, status);
+    }
+    
 
     // MAP_GPIOIntClear(GPIO_PORTN_BASE, GPIO_INT_PIN_4);
 
@@ -38,4 +52,13 @@ void init_zero_crossing(void)
 	MAP_GPIOIntTypeSet(GPIO_PORTN_BASE, GPIO_PIN_4, GPIO_RISING_EDGE);
 	GPIOIntRegister(GPIO_PORTN_BASE, __int_ZERO);
 	MAP_GPIOIntEnable(GPIO_PORTN_BASE, GPIO_INT_PIN_4);
+
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER2);
+	SysCtlDelay(3);
+
+	TimerConfigure(TIMER2_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_ONE_SHOT);
+	TimerPrescaleSet(TIMER2_BASE, TIMER_A, 21);
+	TimerIntEnable(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
+	TimerIntRegister(TIMER2_BASE, TIMER_A, __int_ZERO);
+	TimerLoadSet(TIMER2_BASE, TIMER_A, 44000);
 }
